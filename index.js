f9ee5d26f91e8448a75df6bc12a6450821fd2a77
juliachilikina <julia.chilikina@gmail.com>
2020-04-03 18:44:46 -0700

    Removed 'see more' button

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav"){
       x.className += " responsive";
+      
     } else {
       x.className = "topnav";
     }
+
   }
